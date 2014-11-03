e64609416cb4bb110113ba4942101d4f8e57fe32
Pohao Huang <hr904217@gmail.com>
2014-11-03 14:39:12 +0800

    Add :all :maxpeak :minpeak and fix some issues with attrname and return python dict for onlineFE

--- onlineFE_single.py
+++ onlineFE_single.py
@@ -14,20 +14,26 @@ import numpy
 import csv
 import peakdetect
 
+
 def online_FE(inputfile, featurefile):
 
     # read input from 3 files
     inputraw = pd.read_csv(inputfile)
     tempselected = [line.strip('\n') for line in open(featurefile)]
     selectedfeature = [line.split(':', 1) for line in tempselected]
+    featuresetlist = ['mean','var','skewness','kurtosis','maximum','minimum',
+                      'RMS','std','range','iqr','maxpeak','minpeak','all']
 
     # take the selected attribute out for calculating features
     calculist = list()
     for (attr, feature) in selectedfeature:
-        if attr in inputraw.columns:
-            calculist.append((attr, feature))
-        else:
-            print(attr+" is not in csv file!")
+        for a in inputraw.columns:
+            if a.find(attr)>0  and feature in featuresetlist:
+                calculist.append((a, feature))
+                break
+            elif a == inputraw.columns[-1]:
+                print(attr+'_'+feature+" is not a valid feture!Either attrname"\
+                " or feature is not a valid one.")
 
     if not calculist:
         print("No feature calculated!")
@@ -37,12 +43,79 @@ def online_FE(inputfile, featurefile):
 
     # calculate features
     for (attr, f) in calculist:
-        if f == 'mean':
+        if f == 'all':
             try:
                 result.append(inputraw[attr].mean())
             except:
                 result.append(float('nan'))
-        elif f == 'variance':
+            try:
+                result.append(inputraw[attr].var())
+            except:
+                result.append(float('nan'))
+            try:
+                result.append(inputraw[attr].skew())
+            except:
+                result.append(float('nan'))
+            try:
+                result.append(inputraw[attr].kurt())
+            except:
+                result.append(float('nan'))
+            try:
+                result.append(inputraw[attr].max())
+            except:
+                result.append(float('nan'))
+            try:
+                result.append(inputraw[attr].min())
+            except:
+                result.append(float('nan'))
+            try:
+                result.append(numpy.sqrt(numpy.mean(numpy.square(
+                                         inputraw[attr]))))
+            except:
+                result.append(float('nan'))
+            try:
+                result.append(inputraw[attr].std())
+            except:
+                result.append(float('nan'))
+            try:
+                result.append(inputraw[attr].max()-inputraw[attr].min())
+            except:
+                result.append(float('nan'))
+            try:
+                result.append((inputraw[attr].quantile(0.75) -
+                              inputraw[attr].quantile(0.25))/2)
+            except:
+                result.append(float('nan'))
+            try:
+                maxtab, mintab = peakdetect.peakdet(inputraw[attr],.3)
+                result.append(maxtab[:,1].max())
+            except:
+                result.append(float('nan'))
+            try:
+                maxtab, mintab = peakdetect.peakdet(inputraw[attr],.3)
+                result.append(mintab[:,1].min())
+            except:
+                result.append(float('nan'))
+
+        elif f == 'maxpeak':
+            try:
+                maxtab, mintab = peakdetect.peakdet(inputraw[attr],.3)
+                result.append(maxtab[:,1].max())
+            except:
+                result.append(float('nan'))
+        elif f == 'minpeak':
+            try:
+                maxtab, mintab = peakdetect.peakdet(inputraw[attr],.3)
+                result.append(mintab[:,1].min())
+            except:
+                result.append(float('nan'))
+
+        elif f == 'mean':
+            try:
+                result.append(inputraw[attr].mean())
+            except:
+                result.append(float('nan'))
+        elif f == 'var':
             try:
                 result.append(inputraw[attr].var())
             except:
@@ -57,12 +130,12 @@ def online_FE(inputfile, featurefile):
                 result.append(inputraw[attr].kurt())
             except:
                 result.append(float('nan'))
-        elif f == 'max':
+        elif f == 'maximum':
             try:
                 result.append(inputraw[attr].max())
             except:
                 result.append(float('nan'))
-        elif f == 'min':
+        elif f == 'minimum':
             try:
                 result.append(inputraw[attr].min())
             except:
@@ -90,14 +163,14 @@ def online_FE(inputfile, featurefile):
             except:
                 result.append(float('nan'))
         else:
-            print("Please Enter valid Feature:mean,variance,skewness,kurtosis\
-            ,max,min,RMS,std,range,iqr")
+            print("Please Enter valid Feature:mean,variance,skewness,kurtosis"\
+            ",max,min,RMS,std,range,iqr")
 
     # output
     if not result:
         print("No feature calculated!")
         return
-    os.chdir("E:\\pythonanalysis\\")
+    os.chdir(".\\")
     if sys.version_info >= (3, 0, 0):
         f = open("Result.csv", 'w', newline='')
     else:
@@ -106,12 +179,25 @@ def online_FE(inputfile, featurefile):
     namelist = []
 
     # create the column labels
-    for a, f in selectedfeature:
-        namelist.append(a+"_"+f)
+    for a, f in calculist:
+        if f == 'all':
+            for fl in featuresetlist[:-1]:
+                namelist.append(a+"_"+fl)
+        else:
+            namelist.append(a+"_"+f)
+
+    out = dict()
+    for i in range(0,numpy.size(namelist)):
+        out[namelist[i]] = result[i]
+
     # write labels
     w.writerow(namelist)
     # write values
     w.writerow(result)
+    return out
+
 
 if __name__ == '__main__':
-    online_FE("run0097_Bubblers.Pump-Refill all MO1 Pigtails After Installing New Bubblers__99-current.csv", "attribute_feature.txt")
+    sample = online_FE("run0097_Bubblers.Pump-Refill all MO1 Pigtails After In"\
+    "stalling New Bubblers__99-current.csv", "attribute_feature (1).txt")
+    print(sample)
